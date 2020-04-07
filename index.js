a46f802e0942cc80be972175f9c3afb9d7b1e3b2
castleiMac <castle@tansuyun.cn>
2020-04-08 01:59:35 +0800

    v0.1.1

--- index.js
+++ index.js
@@ -11,99 +11,110 @@ var iconv = require('iconv-lite');
 var Request = require('request');
 var __slice = Array.prototype.slice;
 
-module.exports = function(options) {
+module.exports = function (options) {
   options || (options = {});
-  var request = Request.defaults({ jar: options.jar === true });
+  var request = Request.defaults({
+    jar: options.jar === true
+  });
 
-  if (!(options.host || options.map || options.url)) {
+  if (!(options.host || options.hosts || options.map || options.url)) {
     throw new Error('miss options');
   }
 
-  return function* proxy(next) {
-    var url = resolve(this.path, options);
+  return async function proxy(ctx, next) {
+    let host = options.host;
+    if (options.host instanceof Function) {
+      host = await options.host(ctx, options);
+    }
+    options.hosts = host;
+    var url = resolve(ctx.path, options);
 
-    if(typeof options.suppressRequestHeaders === 'object'){
-      options.suppressRequestHeaders.forEach(function(h, i){
+    if (typeof options.suppressRequestHeaders === 'object') {
+      options.suppressRequestHeaders.forEach(function (h, i) {
         options.suppressRequestHeaders[i] = h.toLowerCase();
       });
     }
 
-    var suppressResponseHeaders = [];  // We should not be overwriting the options object!
-    if(typeof options.suppressResponseHeaders === 'object'){
-      options.suppressResponseHeaders.forEach(function(h, i){
+    var suppressResponseHeaders = []; // We should not be overwriting the options object!
+    if (typeof options.suppressResponseHeaders === 'object') {
+      options.suppressResponseHeaders.forEach(function (h, i) {
         suppressResponseHeaders.push(h.toLowerCase());
       });
     }
 
     // don't match
     if (!url) {
-      return yield* next;
+      return await next;
     }
 
     // if match option supplied, restrict proxy to that match
     if (options.match) {
-      if (!this.path.match(options.match)) {
-        return yield* next;
+      if (!ctx.path.match(options.match)) {
+        return await next;
       }
     }
 
-    var parsedBody = getParsedBody(this);
+    var parsedBody = getParsedBody(ctx);
 
     var opt = {
-      url: url + (this.querystring ? '?' + this.querystring : ''),
-      headers: this.header,
+      url: url + (ctx.querystring ? '?' + ctx.querystring : ''),
+      headers: ctx.header,
       encoding: null,
       followRedirect: options.followRedirect === false ? false : true,
-      method: this.method,
+      method: ctx.method,
       body: parsedBody,
     };
 
     // set 'Host' header to options.host (without protocol prefix), strip trailing slash
-    if (options.host) opt.headers.host = options.host.slice(options.host.indexOf('://')+3).replace(/\/$/,'');
+    if (host) opt.headers.host = host.slice(host.indexOf('://') + 3).replace(/\/$/, '');
 
     if (options.requestOptions) {
       if (typeof options.requestOptions === 'function') {
-        opt = options.requestOptions(this.request, opt);
+        opt = options.requestOptions(ctx.request, opt);
       } else {
-        Object.keys(options.requestOptions).forEach(function (option) { opt[option] = options.requestOptions[option]; });
+        Object.keys(options.requestOptions).forEach(function (option) {
+          opt[option] = options.requestOptions[option];
+        });
       }
     }
 
-    for(name in opt.headers){
-      if(options.suppressRequestHeaders && options.suppressRequestHeaders.indexOf(name.toLowerCase()) >= 0){
+    for (name in opt.headers) {
+      if (options.suppressRequestHeaders && options.suppressRequestHeaders.indexOf(name.toLowerCase()) >= 0) {
         delete opt.headers[name];
       }
     }
 
     if (parsedBody) {
-      var res = yield promisifyRequest(request, opt);
+      var res = await promisifyRequest(request, opt);
     } else {
       // Is there a better way?
       // https://github.com/leukhin/co-request/issues/11
-      var res = yield pipeRequest(this.req, request, Object.assign({ jar: true }, opt));
+      var res = await pipeRequest(ctx.req, request, Object.assign({
+        jar: true
+      }, opt));
     }
 
-    this.status = res.statusCode;
+    ctx.status = res.statusCode;
     for (var name in res.headers) {
       // http://stackoverflow.com/questions/35525715/http-get-parse-error-code-hpe-unexpected-content-length
-      if(suppressResponseHeaders.indexOf(name.toLowerCase())>=0){
+      if (suppressResponseHeaders.indexOf(name.toLowerCase()) >= 0) {
         continue;
       }
       if (name === 'transfer-encoding') {
         continue;
       }
-      this.set(name, res.headers[name]);
+      ctx.set(name, res.headers[name]);
     }
 
     if (options.encoding === 'gbk') {
-      this.body = iconv.decode(res.body, 'gbk');
+      ctx.body = iconv.decode(res.body, 'gbk');
       return;
     }
 
-    this.body = res.body;
+    ctx.body = res.body;
 
     if (options.yieldNext) {
-      yield next;
+      await next;
     }
   };
 };
@@ -112,7 +123,7 @@ function resolve(path, options) {
   var url = options.url;
   if (url) {
     if (!/^http/.test(url)) {
-      url = options.host ? join(options.host, url) : null;
+      url = options.hosts ? join(options.hosts, url) : null;
     }
     return ignoreQuery(url);
   }
@@ -123,21 +134,21 @@ function resolve(path, options) {
     path = options.map(path);
   }
 
-  return (options.host && path) ? join(options.host, path) : null;
+  return (options.hosts && path) ? join(options.hosts, path) : null;
 }
 
 function ignoreQuery(url) {
   return url ? url.split('?')[0] : null;
 }
 
-function getParsedBody(ctx){
+function getParsedBody(ctx) {
   var body = ctx.request.body;
-  if (body === undefined || body === null){
+  if (body === undefined || body === null) {
     return undefined;
   }
   var contentType = ctx.request.header['content-type'];
-  if (!Buffer.isBuffer(body) && typeof body !== 'string'){
-    if (contentType && contentType.indexOf('json') !== -1){
+  if (!Buffer.isBuffer(body) && typeof body !== 'string') {
+    if (contentType && contentType.indexOf('json') !== -1) {
       body = JSON.stringify(body);
     } else {
       body = body + '';
@@ -148,7 +159,7 @@ function getParsedBody(ctx){
 
 function promisifyRequest(request, opt) {
   return new Promise(function (resolve, reject) {
-    request(opt, function(err) {
+    request(opt, function (err) {
       if (err) {
         reject(err);
       } else {
@@ -158,9 +169,9 @@ function promisifyRequest(request, opt) {
   });
 }
 
-function pipeRequest(readable, requestThunk, opt){
+function pipeRequest(readable, requestThunk, opt) {
   return new Promise(function (resolve, reject) {
-    readable.pipe(requestThunk(opt, function(err) {
+    readable.pipe(requestThunk(opt, function (err) {
       if (err) {
         reject(err);
       } else {
